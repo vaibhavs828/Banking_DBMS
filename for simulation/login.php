d977fef3a3bc5fe9225267df858f2f844773be9f
Sudipta Singhal <sudipta482000@gmail.com>
2020-11-27 23:34:55 +0530

    redesigned login page has been made mobile responsive

--- for simulation/login.php
+++ for simulation/login.php
@@ -1,165 +1,72 @@
-<?php
-	session_start();
-	if(array_key_exists("logout",$_GET))
-	{
-		session_destroy();
-		header("location: index.php");
-		
-	}
-	else if((array_key_exists("login", $_SESSION) and $_SESSION["login"]))
-	{
-		header("location: index.php");
-	}
-	$link=mysqli_connect("remotemysql.com","IyUUdMcJn4","XU1HaiAhXC","IyUUdMcJn4");
-	if(mysqli_connect_error())
-	{
-		die ('database connection error');
-	}
-	
-	$string='';
-	$email='';
-	$account_number='';
-	$name='';
-	if(array_key_exists("submit", $_POST))
-	{
-		if(!$_POST['login'] or !$_POST['password'])
-		{
-			$string='<div class="alert alert-danger" role="alert">
-  						Fill the form correctly!!.</div>';
-		}
-		else
-		{
-			$email=$_POST['login'];
-			$query="SELECT account_number,full_name from personal_info where '".$email."'=email";
-			if($result=mysqli_query($link,$query))
-			{
-				$row=mysqli_fetch_array($result);
-				if(!isset($row))
-				{
-					$string='<div class="alert alert-danger" role="alert">
-  						Incorrect LoginID or Password</div>';
-				}
-				else
-				{
-					$account_number=$row[0];
-					$name=$row[1];
-					$query="SELECT password from personal_info where ".$account_number."=account_number";
-					if($result=mysqli_query($link,$query))
-					{
-						$row=mysqli_fetch_array($result);
-						if($row[0]==$_POST['password'])
-						{
-							// $string='<div class="alert alert-success" role="alert">
-  					// 	Welcome back!! You are successfully logged in.</div>';
-							$_SESSION['login']=$account_number;
-							$_SESSION['name']=$name;
-
-
-							$_SESSION['start']= time(); // Taking now logged in time.
-							// // Ending a session in 30 minutes from the starting time.
-							$_SESSION['expire'] = $_SESSION['start'] + (1 * 30);
-							header("location: index.php");
-
-						}
-						else
-						{
-							$string='<div class="alert alert-danger" role="alert">
-  						Incorrect LoginID or Password</div>';
-
-						}
-					}
-
-
-				}
-			}
-
-		}
-
-	}
-	
-
-
-
-
-
-
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<meta charset="utf-8">
-	<!--Google Font-->
-    <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--Google Font-->
+		<link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
 	<title>Login Page</title>
-	<link rel="stylesheet" type="text/css" href="login_style.css">
+	<link rel="stylesheet" href="login_style.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+	
 	<link rel="icon" type="image/png" href="navicon.svg">
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary p-3 ">
-            <!--<div class="container-fluid">-->
-                <a class="navbar-brand" href="index.php" id="nm">
-                    <img src="navicon.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-                    Apna Bank
-                </a>
-                <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php">Home 
-                            </a>
-                        </li>
-                        <li class="nav-item dropdown bg-primary">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-toggle="dropdown" aria-expanded="false">
-                                Services
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="transaction.php">Send Money to own bank</a>
-                                <a class="dropdown-item" href="tootherbank.php">Send Money to other bank</a>
-                                <a class="dropdown-item" href="balance.php">current balance</a>
-                                <a class="dropdown-item" href="#">Raise a Complaint</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="aboutus.php">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">My Account</a>
-                        </li>
-                        
-                        
-                    </ul>
-                </div>
-            <!--</div>-->
-            </div>
-        </nav>
-	<!--<div class="name">
-			<h1>Apna Bank</h1>
-		</div>-->
-	<div class="container" ><?php echo $string?></div>
 
-	<form class="box" method="post">
-		<h1>LOGIN</h1>
-		<input type="text" name="login" placeholder="Enter Email">
-		<input type="password" name="password" placeholder="Enter Password">
-		<input type="submit" name="submit" value="Sign In">
-		<div class="links">
-				<a href="forgotpassword.php">Forgot Password?</a>
+<body>
+	<!-- Image and text for navbar-->
+	<!--<nav class="navbar navbar-dark bg-primary p-3">
+		<a class="navbar-brand" href="index.php" id="nm">
+			<img src="navicon.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+			Apna Bank
+		</a>
+	</nav>-->
+
+	<div></div>
+	<div class="container">
+		<header>LOGIN</header>
+		<form method="post">
+			<div class="input-field">
+				<input type="text" name="login" required>
+				<label>Enter Email</label>
 			</div>
-			<br>
-			<br>
-			<br/>
-			<div class="links">
-				<a href="newAccount.php">Create Account</a>
+			<div class="input-field">
+				<input class="pswrd" type="password" name="password" required>
+				<span class="show">SHOW</span>
+				<label>Enter Password</label>
 			</div>
-	</form>
+			<div class="button" name="submit">
+				<div class="inner">
+				</div>
+				<button type="submit" formaction="index.php">SIGN IN</button>
+			</div>
+		</form>
+
+		<div class="signup">
+			<a href="forgotpassword.php">Forgot Password?</a>
+			<br><br><br><br>
+			<a href="newAccount.php">Create an account</a>
+		</div>
+	</div>
+	<script>
+		var input = document.querySelector('.pswrd');
+		var show = document.querySelector('.show');
+		show.addEventListener('click', active);
+		function active() {
+			if (input.type === "password") {
+				input.type = "text";
+				show.style.color = "#1DA1F2";
+				show.textContent = "HIDE";
+			} else {
+				input.type = "password";
+				show.textContent = "SHOW";
+				show.style.color = "#111";
+			}
+		}
+	</script>
 
 </body>
+
 </html>
