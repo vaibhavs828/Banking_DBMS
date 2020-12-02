384d62480b2acfe41e107e8671cce2ebeed04ee7
jayanth.s <jayanthsibbala123@gmail.com>
2020-12-02 17:57:49 +0530

    home

--- gmail.py
+++ gmail.py
@@ -8,23 +8,33 @@ def send_email():
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login('jayanthsibbala123@gmail.com', 'password')
-    subject=""
-    body=""
+    server.login('jayanthsibbala123@gmail.com','smartpassword')
+    subject="hi hafflu"
+    body = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi!<br>
+       How are you?<br>
+       Here is the <a href="http://www.python.org">link</a> you wanted.
+    </p>
+  </body>
+</html>
+"""
     message=f"Subject: {subject}\n\n{body}"
     #EMAIL
    
-    server.sendmail('jayanthsibbala123@gmail.com', 'reciever email', message)
+    server.sendmail('jayanthsibbala123@gmail.com', 'jayanthsibbala123@gmail.com', message)
     server.quit()
 
 def send_email_at(send_time):
     
     send_email()
     print('email sent')
-    time.sleep(2.592e+6)
+
 
 first_email_time = dt.datetime(2020,12,1,23,7,00) # set your sending time in UTC
-interval = dt.timedelta(minutes=2*60) # set the interval for sending the email
+interval = dt.timedelta(hours=60) # set the interval for sending the email
 
 send_time = first_email_time
 while True:
